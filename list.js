ce4ac4c9d1aa4c405fd8d60a55b33f468817e250
Hongzhi Li <hongzhilig@gmail.com>
2023-03-10 18:26:53 -0700

    Got it to do something

--- list.js
+++ list.js
@@ -1,4 +1,4 @@
-const bookList = [
+const booksInfo = [
     {
         Title: "アズ−ルレ−ンコミックアンソロジ (Azur Lane comic anthology)", Author: "anthology", Volumes: "1-12", NumVol: 12, Completion: "complete",
         Language: "JP", price$: 15
@@ -25,14 +25,42 @@ const bookList = [
     }
 ]
 
-let totalVol = 0; 
+const headers = ["Title", "Author", "Volumes Owned", "Number of Volmues", "Language", "price In $"];
+
+let totalVol = 0;
 let totalPrice = 0;
 
-for (let i = 0; i < bookList.length; i++){
-    totalVol += bookList[i].NumVol;
-    totalPrice += (bookList[i].NumVol * bookList[i].price$)
+for (let i = 0; i < booksInfo.length; i++) {
+    totalVol += booksInfo[i].NumVol;
+    totalPrice += (booksInfo[i].NumVol * booksInfo[i].price$)
 }
 
 document.getElementById("totals").innerHTML = "Total Volumes: " + totalVol + " | Total Price: $" + totalPrice;
 
+function makeTable(data = booksInfo){
+    document.createElement("tr");
+    document.createElement("th");
+    const bookList = document.getElementById("listOfBooks");
+    let row = "";
+    for(let i of data){
+        let string = `<tr>`;
+        string += i.Title + " ";
+        string += i.Author + " ";
+        string += i.Volumes + " ";
+        string += i.NumVol + " ";
+        string += i.Language + " ";
+        string += i.price$;
+        string += "\n";
+        string += '</tr>'
 
+        row += string;
+    }
+    bookList.innerHTML = `<table> 
+        <tr> 
+            <th>${"Title"}</th> 
+        </tr>
+    </table>`;
+    bookList.innerHTML = row;
+}
+
+makeTable(booksInfo);
